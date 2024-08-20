385e2935329df592b2a9917858acd6f92d8ef3ed
gihad <gihadm.mouradpb@gmail.com>
2024-08-20 20:23:19 -0300

    Criando atributo privado, e o seu Setter e Getter

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        Carro onix = new Carro();
+        onix.modelo= "Onix";
+        onix.ano= 2013;
+        onix.cor = "azul petroleo";
+        onix.marca = "Toyota";
 
-
+        onix.setVIN(123456123);
+        System.out.println(onix.getVIN());
     }
 }
